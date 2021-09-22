4723ddc2852f2aa29520f214e15b5455fbeb79ea
Isaac Ameh <67932483+isaacamehgreg@users.noreply.github.com>
2021-09-22 04:04:29 +0100

    started

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const app = express();
 
-
+app.get('/', (req, res) => {
+    res.write("<h1>gcloud hosting works</h1>")
+    res.end;
+})
 
 
 var port = process.env.PORT || 8000;
-app.listen(port, );
+app.listen(port, ()=> console.log('server is on'));
